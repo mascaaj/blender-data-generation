baa7649daa8252dfa21cad5e235503d8c2728f6c
mascaaj_u18dt810 <alokejm@gmail.com>
2021-03-13 16:50:18 -0600

    Add loops to iterate thru required objects in scene

--- src/alphabet_datagen.py
+++ src/alphabet_datagen.py
@@ -31,12 +31,53 @@ def randomly_change_color(material_to_change):
     color.hsv = (hue, 1,1)
     rgba = [color.r,color.g,color.b,1]
     material_to_change.node_tree.nodes["Principled BSDF"].inputs[0].default_value=rgba
-    pass
 
-# Functions to loop through iterations
+# Identify objects to generate images for
+obj_names = ['A','B','C']
+obj_count = len(obj_names)
+
+# Tuple to hold quantities of train test and val datasets
+obj_renders_per_split = [('train',3),('val',2),('test',1)]
+
+# Set output path
+output_path = Path('/dev_ssd/blender/blender-data-generation/data/alphabet_data')
+
+# Use inline python function, get total number of renders
+total_render_count = sum([obj_count * r[1] for r in obj_renders_per_split])
+
+#set render for all objects to false
+for name in obj_names:
+    bpy.context.scene.objects[name].hide_render = True
+
+start_idx = 0
+
+start_time = time.time()
+
+for split_name, renders_per_object in obj_renders_per_split:
+    print(f'Starting split : {split_name} | Total Renders : {renders_per_object * obj_count}')
+    print('========================')
+    
+    for obj_name in obj_names:
+        print(f'Starting object : {split_name}/{obj_name}')
+        print('................................')
+        
+        # Get object and make visible to render
+        obj_to_render = bpy.context.scene.objects[obj_name]
+        obj_to_render.hide_render = False
+        
+        for i in range(start_idx, start_idx+renders_per_object):
+            print(i)
+        
+#        Increment the start image index    
+        start_idx += renders_per_object
+
+
 
 # Functions to output data
 
 #Test Functions
-randomly_rotate_object(bpy.context.scene.objects['B'])
-randomly_change_color(bpy.data.materials["Letter Material"])
+#randomly_rotate_object(bpy.context.scene.objects['B'])
+#randomly_change_color(bpy.data.materials["Letter Material"])
+
+#bpy.context.scene.render.filepath = '/dev_ssd/blender/blender-data-generation/data/alphabet_data/test_render.png'
+#bpy.ops.render.render(write_still=True)
