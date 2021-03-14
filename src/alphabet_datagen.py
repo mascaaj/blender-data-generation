a65eb63bcde0457160df949cb4ad9f9fc89cd1fc
mascaaj_u18dt810 <alokejm@gmail.com>
2021-03-14 11:50:39 -0500

    Make changes to pylintrc and clean src code

--- src/alphabet_datagen.py
+++ src/alphabet_datagen.py
@@ -4,8 +4,8 @@ File to manipulate cad alphabets and generate training data
 import time
 import math
 import random
-from mathutils import Euler, Color
 from pathlib import Path
+from mathutils import Euler, Color
 import bpy
 # Functions to rotate the object & change color
 
@@ -23,7 +23,7 @@ def random_angle():
     return random.random()*2*math.pi
 
 def randomly_change_color(material_to_change):
-    """ 
+    """
     Takes in object and changes its color using hsv
     """
     color = Color()
@@ -55,32 +55,30 @@ start_time = time.time()
 
 for split_name, renders_per_object in obj_renders_per_split:
     print(f'Starting split : {split_name} | Total Renders : {renders_per_object * obj_count}')
-    print('========================')
-    
+    print('====================================')
     for obj_name in obj_names:
         print(f'Starting object : {split_name}/{obj_name}')
         print('................................')
-        
         # Get object and make visible to render
         obj_to_render = bpy.context.scene.objects[obj_name]
         obj_to_render.hide_render = False
-        
         for i in range(start_idx, start_idx+renders_per_object):
             randomly_rotate_object(obj_to_render)
             randomly_change_color(obj_to_render.material_slots[0].material)
-            
             print(f'Rendering image {i+1} of {total_render_count}')
             seconds_for_render = (time.time() - start_time)/(i+1)
             seconds_remaining = ((total_render_count-i-1)*seconds_for_render)
-            print(f'Estimated time remaining : {time.strftime("%H:%M:%S", time.gmtime(seconds_remaining))}')
-            
-            bpy.context.scene.render.filepath= str(output_path / split_name / obj_name / f'{str(i).zfill(6)}.png')
+            print(' ') 
+            print('EstRemaining :') 
+            print(time.strftime("%H:%M:%S", time.gmtime(seconds_remaining)))
+            print(' ') 
+            filepath = str(output_path / split_name / obj_name / f'{str(i).zfill(6)}.png')
+            bpy.context.scene.render.filepath= filepath
             bpy.ops.render.render(write_still=True)
-            
+            print(' ') 
+            print('++++++++++++++++++++++++++++++++')
         obj_to_render.hide_render = True
-
 #        Increment the start image index    
         start_idx += renders_per_object
-
 for name in obj_names:
     bpy.context.scene.objects[name].hide_render = False
